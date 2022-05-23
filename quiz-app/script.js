0583d2ce50bfaed944722b6726109ec8105e4789
davidyfxu <82054510@qq.com>
2022-05-23 22:35:20 +0800

    feat: finish quiz app

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -24,17 +24,22 @@ const quizData = [
     correct: "b",
   },
 ];
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
+const quiz = document.getElementById("quiz");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
+
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+  deselectAnswers();
   const currentQuizData = quizData[currentQuiz];
 
   questionEl.innerHTML = currentQuizData.question;
@@ -43,12 +48,35 @@ function loadQuiz() {
   c_text.innerHTML = currentQuizData.c;
   d_text.innerHTML = currentQuizData.d;
 }
+function getSelected() {
+  let answer;
+
+  answerEls.forEach((answerE1) => {
+    if (answerE1.checked) {
+      answer = answerE1.id;
+    }
+  });
+  return answer;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
 
 submitBtn.addEventListener("click", () => {
-  currentQuiz++;
-  if (currentQuiz < quizData.length) {
-    loadQuiz();
-  } else {
-    alert("finish");
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions</h2>
+      <button onClick="location.reload()">Reload</button>`;
+    }
   }
 });
