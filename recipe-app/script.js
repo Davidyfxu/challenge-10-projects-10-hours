ed62d88809be3e293a309aff8a4cfd42fa58e2c3
davidyfxu <82054510@qq.com>
2022-05-25 23:31:27 +0800

    modify

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,9 +1,16 @@
-function getRandomMeal() {
-  fetch("www.themealdb.com/api/json/v1/1/random.php");
-}
-function getMealById(id) {
-  fetch(`www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-}
-function getMealsBySearch(term) {
-  fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${term}`);
-}
+const getRandomMeal = async () => {
+  const resp = await fetch("www.themealdb.com/api/json/v1/1/random.php");
+  const randomMeal = await resp.json();
+  console.log(randomMeal);
+};
+const getMealById = async (id) => {
+  const meal = await fetch(
+    `www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+};
+const getMealsBySearch = async (term) => {
+  const meals = await fetch(
+    `www.themealdb.com/api/json/v1/1/search.php?s=${term}`
+  );
+};
+getRandomMeal();
